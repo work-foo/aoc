6a9ab105b3ee5eb77877b1e49bcd32bd2a75ed88
Jared Simpson <jared.c.simpson@disney.com>
2024-12-03 00:56:43 -0500

    nested regex

--- src/main/groovy/day03/one.groovy
+++ src/main/groovy/day03/one.groovy
@@ -2,17 +2,14 @@ package day03
 
 import org.codehaus.groovy.tools.groovydoc.ClasspathResourceManager
 
-int acc = 0;
+long acc = 0;
 
-new ClasspathResourceManager().getInputStream('day03/input').splitEachLine(~/ /) { ss ->
-    def ns = ss.collect {it as int}
-    def ds = ns.collate(2,1,false).collect {a, b -> 
-        b-a
-    }
-    if (ds.every {0 < it && it <= 3} || ds.every {-3 <= it && it < 0}) {
-        acc++
-    }
-}
+new ClasspathResourceManager().getInputStream('day03/input')
+        .getText()
+        .eachMatch(~/mul\((\d+),(\d+)\)/) { whole, a, b ->
+            acc += (a as int) * (b as int)
+        }
+
 
 System.out.withPrintWriter { pw -> 
     pw.println(acc)
