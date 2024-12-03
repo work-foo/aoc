b26af6952a296deedd09ba1a752126043179eaed
Jared Simpson <jared.c.simpson@disney.com>
2024-12-03 00:00:23 -0500

    day 3 template

---
+++ src/main/groovy/day03/one.groovy
@@ -0,0 +1,19 @@
+package day03
+
+import org.codehaus.groovy.tools.groovydoc.ClasspathResourceManager
+
+int acc = 0;
+
+new ClasspathResourceManager().getInputStream('day03/input').splitEachLine(~/ /) { ss ->
+    def ns = ss.collect {it as int}
+    def ds = ns.collate(2,1,false).collect {a, b -> 
+        b-a
+    }
+    if (ds.every {0 < it && it <= 3} || ds.every {-3 <= it && it < 0}) {
+        acc++
+    }
+}
+
+System.out.withPrintWriter { pw -> 
+    pw.println(acc)
+}
