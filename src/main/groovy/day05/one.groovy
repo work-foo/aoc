aa675e6b061935b7b798e479d3c45e424431724e
Jared Simpson <jared.c.simpson@disney.com>
2024-12-05 01:38:08 -0500

    part one

--- src/main/groovy/day05/one.groovy
+++ src/main/groovy/day05/one.groovy
@@ -2,14 +2,12 @@ package day05
 
 import org.codehaus.groovy.tools.groovydoc.ClasspathResourceManager
 
-import java.util.regex.Matcher
 import java.util.regex.Pattern
 
-boolean[] expected = [true, true, true, false, false, false]
 long acc = 0
 
 def manager = new ClasspathResourceManager()
-def resource = manager.getInputStream('day05/test')
+def resource = manager.getInputStream('day05/input')
 //def resource = manager.getInputStream('day05/input')
 // right left
 String text = resource.getText()
@@ -18,17 +16,21 @@ Pattern rule = ~/\d\d\|\d\d/, book = ~/\d\d(?:,\d\d)+/
 def rules = text =~ rule
 def edges = rules.iterator().collect { String it ->
     it.split(/\|/)*.toInteger()
-}
+}.toSet()
 
 def books = text =~ book
-def bookpages = books.iterator().collect { String it ->
+acc = books.iterator().collect { String it ->
     it.split(',')*.toInteger()
+}.findAll {
+    it.inits().init().init().every {
+        def last = it.last()
+        !it.init().any {
+            [last, it] in edges
+        }
+    }
+}.sum {
+    it[it.size() / 2]
 }
-
-
-
-while(false);
-
 
 System.out.withPrintWriter { pw ->
     pw.println(acc)
