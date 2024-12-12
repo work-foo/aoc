7c4a8c7b65f02e03bad7ed10694a69835550aff7
Jared Simpson <jared.c.simpson@disney.com>
2024-12-11 22:32:46 -0500

    day 07 part 2 new operator

---
+++ src/main/groovy/day07/two.groovy
@@ -0,0 +1,37 @@
+package day07
+
+import org.codehaus.groovy.tools.groovydoc.ClasspathResourceManager
+
+import java.util.regex.Matcher
+import java.util.regex.Pattern
+
+long acc
+
+def manager = new ClasspathResourceManager()
+//def resource = manager.getInputStream('day07/test')
+def resource = manager.getInputStream('day07/input')
+def lines = resource.readLines()
+Pattern pattern = ~/(\d+): (\d+(?: \d+)*)/
+acc =
+lines.findResults {
+    def matcher = it =~ pattern
+    long testValue = matcher[0][1].toLong()
+    List<Long> numbers = matcher[0][2].split()*.toLong()
+
+    def canSum = numbers.tail().inject([numbers[0]]) { list, val ->
+        def added = list*.plus(val)
+        added.addAll(list*.multiply(val))
+        added.addAll(list.collect { "${it}${val}".toLong()})
+
+        added
+    }.any { it == testValue }
+    canSum ? testValue : 0L
+}.sum()
+
+System.out.withPrintWriter { pw ->
+    pw.println(acc)
+    pw.println(acc == 11387)
+}
+
+
+acc
