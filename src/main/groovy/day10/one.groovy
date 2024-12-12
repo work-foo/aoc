abf645f05c55aedc25be8928e3dddb001c2799d8
Jared Simpson <jared.c.simpson@disney.com>
2024-12-12 15:07:57 -0500

    day 10 init

---
+++ src/main/groovy/day10/one.groovy
@@ -0,0 +1,25 @@
+package day10
+
+import day06.Position
+import org.codehaus.groovy.tools.groovydoc.ClasspathResourceManager
+
+import java.util.function.Supplier
+import java.util.regex.Pattern
+
+long acc
+
+def manager = new ClasspathResourceManager()
+//def resource = manager.getInputStream('day10/test') /* 0099811188827773336446555566 */ ;def expected = 1928
+//def resource = manager.getInputStream('day10/test2'); /* 022111222 */ def expected = 60
+def resource = manager.getInputStream('day10/input'); def expected = 6395800119709
+def text = resource.getText()
+
+
+
+System.out.withPrintWriter { pw ->
+    pw.println(acc)
+    pw.println(acc == expected)
+}
+
+
+acc
