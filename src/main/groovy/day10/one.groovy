8f2b4d03797208ad2ec6dced93c8e2b7ed76065e
Jared Simpson <jared.c.simpson@disney.com>
2024-12-12 20:58:35 -0500

    day 10 dft

--- src/main/groovy/day10/one.groovy
+++ src/main/groovy/day10/one.groovy
@@ -1,18 +1,43 @@
 package day10
 
-import day06.Position
-import org.codehaus.groovy.tools.groovydoc.ClasspathResourceManager
 
-import java.util.function.Supplier
-import java.util.regex.Pattern
+import com.google.common.graph.Traverser
+import org.codehaus.groovy.tools.groovydoc.ClasspathResourceManager
 
 long acc
 
 def manager = new ClasspathResourceManager()
-def resource = manager.getInputStream('day10/test') /* 0099811188827773336446555566 */ ;def expected = 1
+//def resource = manager.getInputStream('day10/test') /* 0099811188827773336446555566 */ ;def expected = 1
+//def resource = manager.getInputStream('day10/test3') /* 0099811188827773336446555566 */ ;def expected = 1
 //def resource = manager.getInputStream('day10/test2'); /* 022111222 */ def expected = 36
-//def resource = manager.getInputStream('day10/input'); def expected = 6395800119709
-def text = resource.getText()
+def resource = manager.getInputStream('day10/input'); def expected = 786
+def text = resource.withCloseable {it.readLines()}
+def depth = text.size(), width = text[0].length()
+def onboard = { d, r -> d in 0..<depth && r in 0..<width }
+record Node(int down, int right, int value) {}
+
+List<Node> zeroes = []
+text.eachWithIndex { String row, int d ->
+    row.toCharArray().eachWithIndex { char height, int r ->
+        def node = new Node(d, r, Character.getNumericValue(height))
+        if (node.value()==0) zeroes << node
+    }
+}
+
+
+def traverser = Traverser.forGraph { Node n ->
+    def adja = [-1, 1]*.plus(n.down()).collect { [it, n.right()] } + [-1, 1]*.plus(n.right()).collect { [n.down(), it] }
+
+    adja.grep(onboard).collect { d, r ->
+        new Node(d, r, Character.getNumericValue(text[d].charAt(r)))
+    }.grep {
+        n.value()+1 == it.value()
+    }
+}
+
+acc=
+zeroes.sum {traverser.depthFirstPreOrder(it).count { it.value() == 9 } }
+
 
 
 
