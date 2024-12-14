84c3708a0250523a89028ee8717a4af985be3ea3
Jared Simpson <jared.c.simpson@disney.com>
2024-12-14 03:16:57 -0500

    day 13 part 1

--- src/main/groovy/day13/one.groovy
+++ src/main/groovy/day13/one.groovy
@@ -3,28 +3,31 @@ package day13
 import com.google.common.graph.Traverser
 import org.codehaus.groovy.tools.groovydoc.ClasspathResourceManager
 
-long acc
+BigInteger acc = 0
 
 def manager = new ClasspathResourceManager()
 //def resource = manager.getInputStream('day13/test')  ;def expected = 480
 //def resource = manager.getInputStream('day13/test2') ;def expected = 772
 //def resource = manager.getInputStream('day13/test3') ;def expected = 18*4
-def resource = manager.getInputStream('day13/input'); def expected = 193899
-def lines = resource.getText().eachMatch(~/(?s)(?:\D*)(\d+)(?:\D*)(\d+)(?:\D*)(\d+)(?:\D*)(\d+)(?:\D*)(\d+)(?:\D*)(\d+)/) { whole, sax, say, sbx, sby, spx, spy ->
-    def ax = sax as int
-    def ay = say as int
-    def bx = sbx as int
-    def by = sby as int
-    def px = spx as int
-    def py = spy as int
+def resource = manager.getInputStream('day13/input'); def expected = 36838
+def lines = resource.getText().eachMatch(~/(?s)(?:\D*)(\d+)(?:\D*)(\d+)(?:\D*)(\d+)(?:\D*)(\d+)(?:\D*)(\d+)(?:\D*)(\d+)/) { whole, String sax, String say, String sbx, String sby, String spx, String spy ->
+    def ax = sax.toBigInteger()
+    def ay = say.toBigInteger()
+    def bx = sbx.toBigInteger()
+    def by = sby.toBigInteger()
+    def px = spx.toBigInteger()//+ BigInteger.valueOf(10000000000000L)
+    def py = spy.toBigInteger()//+ BigInteger.valueOf(10000000000000L)
 
-    def det = ax * by - ay * bx
+    def det = (ax * by - ay * bx)
     def a = by*px - bx*py
     def b = -ay*px + ax*py
 
+    println det
     if (det != 0) {
-        if (a % det == 0 && b % det == 0) {
-            acc += a / det * 3 + b / det
+        if (a.abs().mod(det.abs()) == 0 && b.abs().mod(det.abs()) == 0) {
+            def cost = a.divide(det)*3 + b.divide(det)
+            println cost
+            acc += cost
         }
     } else { //if (px % ax == 0 && py % ay == 0 && px*ay == ax*py) {
         // singular
