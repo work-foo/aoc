24f0eaaad2b5f1c5c4798db0272dfdb84e9afb9f
Jared Simpson <jared.c.simpson@disney.com>
2024-12-18 20:16:21 -0500

    day 17 part2 comments

---
+++ src/main/groovy/day17/two.groovy
@@ -0,0 +1,98 @@
+package day17
+
+
+import org.codehaus.groovy.tools.groovydoc.ClasspathResourceManager
+
+StringJoiner acc = new StringJoiner(',')
+
+def manager = new ClasspathResourceManager()
+//def resource = manager.getInputStream('day17/test')  ;def expected = '4,6,3,5,6,3,5,2,1,0'
+//def resource = manager.getInputStream('day17/test2') ;def expected = 11048
+//def resource = manager.getInputStream('day17/test3') ;def expected = 14+1000+4
+def resource = manager.getInputStream('day17/input'); def expected = '2,4,1,3,7,5,4,0,1,3,0,3,5,5,3,0'
+def lines = resource.readLines()
+int blank = lines.findIndexOf {it.empty}
+def registers = lines.take(blank).findResults { (it =~ /\d+/)[0].toInteger()}
+def program = (lines.get(blank+1) =~ /\d(?:,\d)+$/)[0].split(',')*.toInteger()
+
+int ip = 0
+
+def combo = {
+    if (it <= 3) it
+    else if (it <= 6) registers[it-4]
+    else 0
+}
+while (ip < program.size()) {
+    def opcode = program[ip]
+    def operand = program[ip+1]
+    def comboOperand = combo(operand)
+    def shiftA = registers[0] >> Math.min(31, comboOperand )
+    def xorB = registers[1] ^ comboOperand
+    def opMod = comboOperand % 8
+    def anz = registers[0] != 0
+    def xorBC = registers[1] ^ registers[2]
+
+    switch (opcode) {
+        case 0:
+            registers[0] = shiftA
+            break
+        case 1:
+            registers[1] = xorB
+            break
+        case 2:
+            registers[1] = opMod
+            break
+        case 4:
+            registers[1] = xorBC
+            break
+        case 5:
+            acc.add(opMod.toString())
+            break
+        case 6:
+            registers[1] = shiftA
+            break
+        case 7:
+            registers[2] = shiftA
+            break
+        default: break
+    }
+    if (opcode == 3 && anz) ip = comboOperand
+    else ip += 2
+
+    println registers
+    println acc.toString()
+
+}
+
+/*
+* 3 bit ops
+* 3 registers unbounded
+* instruction is 0..7 followed by operand
+* ip += 2
+* read beyond halt
+* operand parsed by opcode
+* literal op is value
+* combo op 01234567
+* combo op 0123ABC.
+*
+* 0adv : A = shift A op
+* 1bxl : B = xor B op
+* 2bst : B = op % 8
+* 3jnz : A<>0 then ip = op
+* 4bxc : B = xor B C
+* 5out : print op % 8
+* 6bdv : B = shift A op
+* 7cdv : C = shift A op
+*
+*
+*
+* */
+
+
+System.out.withPrintWriter { pw ->
+    pw.println(acc)
+    pw.println(acc.toString() == expected)
+}
+
+
+acc
